2ec6719fbb54c8ef2fef230799c099bf638dfaff
Gabriella Bueno <gabriellacbueno@outlook.com>
2024-07-01 19:31:33 -0300

    projeto ContaTerminal

---
+++ ContaBanco/src/Conta.java
@@ -0,0 +1,20 @@
+public class Conta {
+    
+    int numeroConta;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public Conta(int numeroConta, String agencia, String nomeCliente, double saldo) {
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;        
+    }
+
+    public void mensagemSaudacao() {
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! \n\n" +
+                            "Sua agência é " + agencia + ", conta " + numeroConta + 
+                            " e seu saldo " + String.format("R$ %,.2f", saldo) + " já está disponível para saque.");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# java-backend-bootcamp
+# Santander Bootcamp 2024 - Backend com Java
+
+Este repositório contém os projetos práticos que desenvolvi durante o Santander Bootcamp 2024 na trilha de **Back-end com Java**. 
+Aqui, você encontrará uma variedade de aplicações e exercícios que demonstram minhas habilidades desenvolvendo com Java.
+
